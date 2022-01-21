220b5cf5a219eededac1a54f13253e96915bff4e
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-22 00:02:12 +0100

    MAde function for moving it with mouse

--- site/public/scripts/drawGrid.js
+++ site/public/scripts/drawGrid.js
@@ -1,26 +1,23 @@
 const drawGrid = (canvas, ctx, cellSize) => {
-  const left = 0;
-  const top = 0;
-  const right = canvas.width;
-  const bottom = canvas.height;
+  const left = 0 - canvas.width * 4;
+  const top = 0 - canvas.height * 4;
+  const right = canvas.width * 4;
+  const bottom = canvas.height * 4;
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(left, top, right - left, bottom - top);
   ctx.translate(0.5, 0.5);
   ctx.beginPath();
 
-  for (let x = 0; x < right; x += cellSize) {
+  for (let x = left; x < right; x += cellSize) {
     ctx.moveTo(x, top);
     ctx.lineTo(x, bottom);
   }
-  for (let y = 0; y < bottom; y += cellSize) {
+  for (let y = top; y < bottom; y += cellSize) {
     ctx.moveTo(left, y);
     ctx.lineTo(right, y);
   }
-
-  ctx.lineWidth = 1;
   ctx.strokeStyle = "white";
   ctx.stroke();
-  console.log("Grid srawn");
 };
 
 export default drawGrid;
--- site/public/scripts/index.js
+++ site/public/scripts/index.js
@@ -7,3 +7,33 @@ canvas.width = window.innerWidth / 2;
 const ctx = canvas.getContext("2d");
 
 drawGrid(canvas, ctx, 10);
+
+let start;
+const getPos = (e) => ({
+  x: e.clientX - canvas.offsetLeft,
+  y: e.clientY - canvas.offsetTop,
+});
+
+const reset = () => {
+  start = null;
+  ctx.setTransform(1, 0, 0, 1, 0, 0); // reset translation
+  drawGrid(canvas, ctx, 10);
+};
+
+canvas.addEventListener("mousedown", (e) => {
+  reset();
+  start = getPos(e);
+});
+
+canvas.addEventListener("mouseup", reset);
+canvas.addEventListener("mouseleave", reset);
+
+canvas.addEventListener("mousemove", (e) => {
+  // Only move the grid when we registered a mousedown event
+  if (!start) return;
+  const pos = getPos(e);
+  // Move coordinate system in the same way as the cursor
+  ctx.translate(pos.x - start.x, pos.y - start.y);
+  drawGrid(canvas, ctx, 10);
+  start = pos;
+});
