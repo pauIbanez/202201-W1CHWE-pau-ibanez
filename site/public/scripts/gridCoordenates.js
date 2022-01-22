ddc99ffe8669a8b7ddf5d08e092e8d89cb3f6a86
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-22 16:02:28 +0100

    Refactored getGridCoords function to getr correct cell position

--- site/public/scripts/gridCoordenates.js
+++ site/public/scripts/gridCoordenates.js
@@ -2,20 +2,16 @@ const getGridAlignedCoords = (coordenates) => {
   const newCoordenates = coordenates;
 
   if (coordenates.x % 10 !== 0) {
-    newCoordenates.x = Math.round(coordenates.x / 10) * 10;
+    newCoordenates.x = Math.floor(coordenates.x / 10) * 10;
   }
 
   if (coordenates.y % 10 !== 0) {
-    newCoordenates.y = Math.round(coordenates.y / 10) * 10;
+    newCoordenates.y = Math.floor(coordenates.y / 10) * 10;
   }
   console.log(newCoordenates);
   return newCoordenates;
 };
 
-const getMouseToGridCoordenates = (coordenates) => {
-  console.log(coordenates);
-};
-
 export default getGridAlignedCoords;
 
-export { getGridAlignedCoords, getMouseToGridCoordenates };
+export { getGridAlignedCoords };
