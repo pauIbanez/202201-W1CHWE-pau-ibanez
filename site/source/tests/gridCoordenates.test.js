9943b0e235afb701f41bd92e05b69208d84b3efc
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-24 00:24:08 +0100

    Added first test for getGridAlignedCoordenates

--- site/source/tests/gridCoordenates.test.js
+++ site/source/tests/gridCoordenates.test.js
@@ -1,1 +1,15 @@
+import { getGridAlignedCoords } from "../../public/scripts/gridCoordenates";
 
+describe("Given getGridAlingedCoordenates", () => {
+  describe("When inputed {x:57, y:84}", () => {
+    test("It should return {x:40, y:80}", () => {
+      const coordsToGive = { x: 57, y: 84 };
+      const cellSize = 20;
+      const expectedResult = { x: 40, y: 80 };
+
+      const result = getGridAlignedCoords(coordsToGive, cellSize);
+
+      expect(JSON.stringify(result)).toBe(JSON.stringify(expectedResult));
+    });
+  });
+});
