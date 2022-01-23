d3d958cad4abfa74e87a4d3dcf534ac86839a95e
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-23 13:25:30 +0100

    Moved runGame function to gameRunner.js

--- site/public/scripts/gameRunner.js
+++ site/public/scripts/gameRunner.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/extensions */
 import { calculateNextGen } from "./gameStateCalculator.js";
 import { drawCell } from "./cellRendering.js";
+import { watchlistGenerator } from "./watchlist.js";
 
 const runNextGen = (cells, cellsPositions, ctx, canvas) => {
   const { cellSize } = cells[0];
@@ -14,5 +15,13 @@ const runNextGen = (cells, cellsPositions, ctx, canvas) => {
   return nextGenCells;
 };
 
-export default runNextGen;
-export { runNextGen };
+const runGame = (ctx, canvas, cellPositions, cells, cellSize, speed) => {
+  const intervalId = setInterval(() => {
+    cells = watchlistGenerator(cellPositions, cellSize);
+    cellPositions = runNextGen(cells, cellPositions, ctx, canvas);
+  }, speed);
+  return intervalId;
+};
+
+export default runGame;
+export { runGame };
