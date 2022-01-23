3e4b91a74231da9c1b26c016d1b9be9c4685a213
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-23 23:59:20 +0100

    Added tests for watchlistGenerator

--- site/source/tests/watchlist.test.js
+++ site/source/tests/watchlist.test.js
@@ -3,7 +3,7 @@ import { watchlistGenerator } from "../../public/scripts/watchlist";
 describe("Given watchlistGenerator", () => {
   describe("When inputed [{x:10, y:10}], 20", () => {
     test("Then it should return 9 watchlists", () => {
-      const cellsToGive = [{ x: 10, y: 10 }];
+      const cellsToGive = [{ x: 20, y: 20 }];
       const cellSize = 20;
       const expectedLenght = 9;
 
@@ -13,18 +13,53 @@ describe("Given watchlistGenerator", () => {
     });
   });
 
-  describe("When inputed [{x:10, y:10}, {x:40, y:40}], 20", () => {
+  describe("When inputed [{x:20, y:20}, {x:40, y:40}], 20", () => {
     test("Then it should return 18 watchlists", () => {
       const cellsToGive = [
-        { x: 10, y: 10 },
+        { x: 20, y: 20 },
         { x: 40, y: 40 },
       ];
       const cellSize = 20;
-      const expectedLenght = 18;
+      const expectedLenght = 14;
 
       const result = watchlistGenerator(cellsToGive, cellSize);
 
       expect(result.length).toBe(expectedLenght);
+    });
+  });
+
+  describe("When inputed [{x:10, y:10}, {x:30, y:30}], 20", () => {
+    test("Then it should return 17 watchlists", () => {
+      const cellsToGive = [
+        { x: 20, y: 20 },
+        { x: 60, y: 60 },
+      ];
+      const cellSize = 20;
+      const expectedLenght = 17;
+
+      const result = watchlistGenerator(cellsToGive, cellSize);
+      expect(result.length).toBe(expectedLenght);
+    });
+  });
+  describe("When inputed [{x:10, y:10}, {x:30, y:30}], 20", () => {
+    test("Then it should return 17 watchlists", () => {
+      const cellsToGive = [{ x: 20, y: 20 }];
+      const cellSize = 20;
+      const expectedCellToBeAlive = 4;
+
+      const result = watchlistGenerator(cellsToGive, cellSize);
+      expect(result[expectedCellToBeAlive].alive).toBe(true);
+    });
+  });
+
+  describe("When inputed [{x:10, y:10}, {x:30, y:30}], 20", () => {
+    test("Then it should return 17 watchlists", () => {
+      const cellsToGive = [{ x: 20, y: 20 }];
+      const cellSize = 20;
+      const expectedCellToBeDead = 8;
+
+      const result = watchlistGenerator(cellsToGive, cellSize);
+      expect(result[expectedCellToBeDead].alive).toBe(false);
     });
   });
 });
