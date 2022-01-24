96135e6d85dd652b6343af75f925eeb91aa2755c
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-24 01:22:08 +0100

    Added yet another test checking the acual results

--- site/source/tests/gameStateCalculator.test.js
+++ site/source/tests/gameStateCalculator.test.js
@@ -31,4 +31,25 @@ describe("Given calculateNextGen", () => {
       expect(result.length).toBe(expectedArraySize);
     });
   });
+
+  describe("When inputed 3 cells with origin {x:20, y:0}, {x:20, y:20} and {x:20, y:40} ", () => {
+    test("Then it should return and array with {x:0, y:20}, {x:20, y:20} and {x:40, y:20}", () => {
+      const cellToCreate = [
+        { x: 20, y: 0 },
+        { x: 20, y: 20 },
+        { x: 20, y: 40 },
+      ];
+      const cellSize = 20;
+      const prevGenCells = watchlistGenerator(cellToCreate, cellSize);
+      const expectedArray = [
+        { x: 0, y: 20 },
+        { x: 20, y: 20 },
+        { x: 40, y: 20 },
+      ];
+
+      const result = calculateNextGen(prevGenCells, cellToCreate);
+
+      expect(result).toStrictEqual(expectedArray);
+    });
+  });
 });
